53e63d3b1b0484f9fba76846ce0dd00db99f1f39
Nikita V <swebs.sh@gmail.com>
2024-06-03 10:11:46 +0500

    Установил inertia vue 3

---
+++ resources/js/Pages/test.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 style="text-align: center; margin: 50px 0">
+            Происходит перенос сайта Intelab
+        </h1>
+    </div>
+</template>
